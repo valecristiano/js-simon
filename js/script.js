d38d156ea3dc21257ab858ff3398c7a71f22c2fb
valecristiano <valeriamdcristiano@gmail.com>
2026-01-14 16:32:58 +0300

    casual number start

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 // Visualizzare in pagina 5 numeri casuali.
+const casualNumbersList = document.getElementById("numbers-list");
+console.log(casualNumbersList);
+
+function generateCasualNumber(min, max) {
+  let number = Math.floor(Math.random() * (max - min + 1)) + min;
+  return number;
+}
+
+let casualNumbers = generateCasualNumber(1, 50);
+console.log(casualNumbers);
+
+casualNumbersList.innerText = casualNumbers;
+
 // - Da lì parte un timer di 30 secondi.
 // - Dopo 30 secondi i numeri scompaiono
 // - Appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente,
